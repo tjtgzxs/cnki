ebccf62841424a9c89fd2e67fdb3cbd54ca7e0ad
zhangxiaoshen <387753733@qq.com>
2021-10-25 16:00:00 +0800

    detail parse

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+
+
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
@@ -7,7 +9,7 @@ from selenium.webdriver.firefox.options import Options
 import time
 from selenium.webdriver.chrome.service import Service
 from io import StringIO
-
+from selenium.webdriver.support import expected_conditions as EC
 from pdfminer.converter import TextConverter
 from pdfminer.layout import LAParams
 from pdfminer.pdfdocument import PDFDocument
@@ -19,12 +21,13 @@ def open_url():
     firefox_options = Options()
     # firefox_options.add_argument("--headless")
     # firefox_options.add_argument("User-Agent: Mozilla/5.0 (X11; Linux x86_64; rv:93.0) Gecko/20100101 Firefox/93.0")
-    driver = Firefox("./",options=firefox_options)
-
+    driver = Firefox(executable_path="./geckodriver",options=firefox_options)
+    # driver=Firefox()
     # driver.get('https://bot.sannysoft.com/')
     # time.sleep(5)
     # driver.save_screenshot('walkaround.png')
     driver.get("https://www.cnki.net/")
+    time.sleep(5)
     a = driver.find_element_by_class_name('sort-default')
     webdriver.ActionChains(driver).move_to_element(a).click(a).perform()
     driver.find_element_by_link_text("全文").click()  # 选择高级搜索这个选项，并点击
@@ -32,12 +35,26 @@ def open_url():
     time.sleep(2)
     a = driver.find_element_by_class_name('search-btn')
     a.click()
+    time.sleep(10)
     ch = driver.find_element_by_class_name("ch")
     ch.click()
     time.sleep(10)
     xsqk = driver.find_element_by_xpath("//ul[@class='doctype-menus keji']/li[1]/a/span")
     xsqk.click()
     time.sleep(10)
+    page_a = driver.find_element_by_id('perPageDiv')
+    webdriver.ActionChains(driver).move_to_element(page_a).click(page_a).perform()
+    driver.find_element_by_link_text("50").click()  #
+    time.sleep(20)
+    next_btn=driver.find_element_by_id('PageNext')
+    get_detail(driver)
+    while(next_btn.is_enabled()):
+        next_btn.click()
+        time.sleep(20)
+        get_detail(driver)
+
+
+def get_detail(driver):
     tr_list = driver.find_elements_by_xpath("//table[@class='result-table-list']/tbody/tr")
     for tr in tr_list:
         name = tr.find_element_by_class_name("name").text
@@ -46,10 +63,19 @@ def open_url():
         date = tr.find_element_by_class_name("date").text
         quote = tr.find_element_by_class_name("quote").text
         download = tr.find_element_by_class_name("download").text
-        handles = driver.window_handles
-        driver.switch_to.window(handles[1])
         print(name)
-    time.sleep(50)
+
+        window=driver.current_window_handle
+        detail_button=tr.find_element_by_class_name("name").click()
+        time.sleep(50)
+        windows = driver.window_handles
+        detail=driver.switch_to.window(windows[-1])
+        print( driver.find_element_by_id("ChDivSummary").text)
+        driver.close()
+        driver.switch_to.window(window)
+
+    time.sleep(10)
+
 
 def PDFHandle():
     output_string = StringIO()
@@ -91,8 +117,8 @@ def re_find(value):
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    value=PDFHandle()
-    print(re_find("大苏打飒飒的,第5期CGSS的具体呢荣，哈哈哈哈"))
+    value=open_url()
+    # print(re_find("大苏打飒飒的,第5期CGSS的具体呢荣，哈哈哈哈"))
 
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
