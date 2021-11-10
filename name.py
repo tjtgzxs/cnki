3ca8c1a5ff12f0ee903c112b552e42cdcf327317
zhangxiaoshen <387753733@qq.com>
2021-11-10 17:11:49 +0800

    first finish

--- name.py
+++ name.py
@@ -4,7 +4,22 @@ BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 def change_name():
     pdf_dir=BASE_DIR+os.sep+"pdf"+os.sep
     pdf_list = os.listdir(pdf_dir)
-    return  pdf_list
+    for file in pdf_list:
+
+        pdf_extend = os.path.splitext(file)[-1]
+        pdf_name = os.path.splitext(file)[0]
+        old_name = pdf_dir + file
+        tmp_name=pdf_name.replace("\"","")
+        tmp_name = tmp_name.replace("'", "")
+        tmp_name=tmp_name.replace("-","")
+        tmp_name=tmp_name.replace(";","")
+        # tmp_name=tmp_name.replace("")
+        new_name = pdf_dir + tmp_name + pdf_extend
+        try:
+            os.rename(old_name, new_name)
+        except:
+            continue
+
 
 
 if __name__=="__main__":
--- test.py
+++ test.py
@@ -1,4 +1,4 @@
-from selenium import webdriver
+from seleniumwire  import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.options import Options
 import requests
@@ -11,14 +11,19 @@ def getCode(im_path,type=1902):
     im = open(im_path, 'rb').read()
     return chaojiying.PostPic(im, type)
 # session=requests.session()
-# img_src=session.get("https://kns.cnki.net/KNS8/Brief/VerifyCode?t=589aaffd-354c-475b-8e15-4828f3889abdhttps://kns.cnki.net/KNS8/Brief/VerifyCode?t=589aaffd-354c-475b-8e15-4828f3889abdhttps://kns.cnki.net/KNS8/Brief/VerifyCode?t=589aaffd-354c-475b-8e15-4828f3889abdhttps://kns.cnki.net/KNS8/Brief/VerifyCode?t=589aaffd-354c-475b-8e15-4828f3889abd")
+# headers={
+#     "Cookie":"Ecp_ClientId=2211110090501201662; Ecp_IpLoginFail=21111060.30.241.14; ASP.NET_SessionId=yl32p1yga44gjzp1io1zechg; SID_kns8=123124; cnkiUserKey=0972c8ce-f9b9-88d7-6388-70a890e7d039; CurrSortField=%e7%9b%b8%e5%85%b3%e5%ba%a6%2f(ffd%2c%27rank%27); CurrSortFieldType=DESC; Ecp_ClientIp=60.30.241.14; SID_kns_new=kns123114; dstyle=listmode; dSearchFold=undefined; dsorder=pubdate; language=undefined; knsLeftGroupSelectItem=; searchTimeFlag=0; dperpage=20; _pk_ref=%5B%22%22%2C%22%22%2C1636509134%2C%22https%3A%2F%2Fwww.cnki.net%2F%22%5D; _pk_ses=*; _pk_id=dacc45ca-a7cb-4220-ba29-e47e50e11010.1636506299.2.1636511788.1636507274."
+# }
+# img_src=session.get("https://kns.cnki.net/KNS8/Brief/VerifyCode?t=cdc9bd6d-d84b-4841-839d-4ac5e45a97ba",headers=headers)
 # with open('code.jpg', 'wb') as f:
 #     f.write(img_src.content)
-# code = getCode('code.jpg')
+# code = getCode('example.png')
 # print(code)
 chrome_options = Options()
 # chrome_options.add_argument('--headless')
 chrome_options.add_argument('--no-sandbox')
 driver = webdriver.Chrome(executable_path=ChromeDriverManager().install(),options=chrome_options)
 driver.get("https://www.cnki.net/")
+for request in driver.requests:
+    print(request.headers)
 
